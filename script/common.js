25498dd5912872e53342e753ab0e05c4d87468c3
Brave <august8821@naver.com>
2024-03-08 18:44:42 +0900

    중간저장 저녁!

---
+++ script/common.js
@@ -0,0 +1,66 @@
+/* nav 탭 js */
+
+/* 관람 & 참여 a click - > ↓ div 열리게. */
+/* art_view_lnb.style.display = "none"  */
+const art_view_a = document.querySelector(".art_view_a")
+const display_a = document.querySelector(".display_a")
+const education_a = document.querySelector(".education_a")
+const collection_a = document.querySelector(".collection_a")
+const art_research_a = document.querySelector(".art_research_a")
+const digital_a = document.querySelector(".digital_a")
+const exhibition_a = document.querySelector(".exhibition_a")
+
+/* 관람 & 참여 ~ div */
+/* (".art_view_lnb ul") */
+const art_view_lnb = document.querySelector(".art_view_lnb")
+const display_lnb = document.querySelector(".display_lnb")
+const education_lnb = document.querySelector(".education_lnb")
+const collection_lnb = document.querySelector(".collection_lnb")
+const art_research_lnb = document.querySelector(".art_research_lnb")
+const digital_lnb = document.querySelector(".digital_lnb")
+const exhibition_lnb = document.querySelector(".exhibition_lnb")
+
+/* 같은게 아니니까 각  */
+title_tap_btn.forEach((t,i)=>{
+    
+})
+
+
+
+
+
+
+
+/* tap click */
+/* 전시 a탭 클릭시 색 변환 */
+/* display_btn -> a 탭 전체 */
+const title_tap_btn = document.querySelectorAll(".a")
+
+/* 초기화 -> 탭을 누르지 않은 a */
+const title_tap_btn_reset = () => {
+    for(let i of title_tap_btn)
+    {
+        i.classList.remove
+        ("title_tap_style")
+        i.style.color = "initial";
+        i.style.border = "initial";
+    }
+
+    for(let i of title_tap_btn){
+        i.style.color = "#949494"
+    }
+}
+title_tap_btn_reset()
+
+/* 탭 클릭 시  a*/
+title_tap_btn.forEach((t,i)=>{
+    t.addEventListener("mousemove",(e)=>{
+        e.preventDefault()
+        title_tap_btn_reset()
+        t.style.color = "#000"
+        t.style.borderBottom = "1px solid #000"
+    })
+})
+
+/* ------------------------------------- */
+
--- script/index.js
+++ script/index.js
@@ -4,23 +4,27 @@ window.addEventListener("scroll", function() {
     wrap_header_scroll.classList.toggle("sticky", window.scrollY > 0);
 });
 
+/*  */
+
+
+
 /* 전시 탭 변환 */
 /* 전체 */
-display_tap_btn1 = document.querySelector(".display_tap_btn1")
-display_tap_btn2 = document.querySelector(".display_tap_btn2")
-display_tap_btn3 = document.querySelector(".display_tap_btn3")
-display_tap_btn4 = document.querySelector(".display_tap_btn4")
-display_tap_btn5 = document.querySelector(".display_tap_btn5")
-display_tap_btn6 = document.querySelector(".display_tap_btn6")
+const display_tap_btn1 = document.querySelector(".display_tap_btn1")
+const display_tap_btn2 = document.querySelector(".display_tap_btn2")
+const display_tap_btn3 = document.querySelector(".display_tap_btn3")
+const display_tap_btn4 = document.querySelector(".display_tap_btn4")
+const display_tap_btn5 = document.querySelector(".display_tap_btn5")
+const display_tap_btn6 = document.querySelector(".display_tap_btn6")
 
 
 /* 각각 탭 스와이퍼 컨테이너 컨테이너 */
-display_image1 = document.querySelector(".display_image1")
-display_image2 = document.querySelector(".display_image2")
-display_image3 = document.querySelector(".display_image3")
-display_image4 = document.querySelector(".display_image4")
-display_image5 = document.querySelector(".display_image5")
-display_image6 = document.querySelector(".display_image6")
+const display_image1 = document.querySelector(".display_image1")
+const display_image2 = document.querySelector(".display_image2")
+const display_image3 = document.querySelector(".display_image3")
+const display_image4 = document.querySelector(".display_image4")
+const display_image5 = document.querySelector(".display_image5")
+const display_image6 = document.querySelector(".display_image6")
 
 display_image2.style.display = "none" 
 display_image3.style.display = "none" 
@@ -76,7 +80,6 @@ display_tap_btn4.addEventListener("click", () => {
     display_image5.style.display = "none"
     display_image6.style.display = "none"
     event.preventDefault();
-    console.log("성공?")
 }); 
 
 /* display_tap_btn5 - 청주 -  */
@@ -103,25 +106,6 @@ display_tap_btn6.addEventListener("click", () => {
     console.log("성공?")
 }); 
 
-// 전시 탭 버튼
-/* const display_tap_btns = document.querySelectorAll(".display_bnr_list li a");
-
-// 모든 이미지 컨테이너
-const display_image_containers = document.querySelectorAll(".display_image");
-
-// 각 탭에 대한 이벤트 리스너 등록
-display_tap_btns.forEach((btn, index) => {
-    btn.addEventListener("click", () => {
-        // 모든 이미지를 숨김
-        display_image_containers.forEach(container => {
-            container.style.display = "none";
-        });
-        // 클릭한 탭에 해당하는 이미지만 표시
-        display_image_containers[index].style.display = "block";
-    });
-});
- */
-
 
 /* 
 Overall,
@@ -132,32 +116,58 @@ Gwacheon,
 Seoul
 */
 
+
+/* 전시 a탭 클릭시 색 변환 */
+/* display_btn -> a 탭 전체 */
+const display_btn = document.querySelectorAll(".display_tap")
+
+/* 초기화 -> 탭을 누르지 않은 a */
+const display_btn_reset = () => {
+    for(let i of display_btn)
+    {
+        i.classList.remove
+        ("display_tap_style")
+        i.style.color = "initial";
+        i.style.border = "initial";
+    }
+
+    for(let i of display_btn){
+        i.style.color = "#949494"
+    }
+}
+display_btn_reset()
+/* 탭 클릭 시  a*/
+display_btn.forEach((t,i)=>{
+    t.addEventListener("click",(e)=>{
+        e.preventDefault()
+        display_btn_reset()
+        t.style.color = "#000"
+        t.style.borderBottom = "1px solid #000"
+        
+    })
+})
+
+/* 전시 스와이퍼 */
+
 /* 전시 전체 */
-const my_swiper_overall = new Swiper('.display_btm_container', { 
+const my_swiper_overall = new Swiper('.display_image1', { 
 /*     autoplay:{delay:5000}, */
-    slidesPerView: 4, //슬라이드 한번에 1개씩 보이기
-    /* spaceBetween:10, // 슬라이드 여백 */
-    /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
-    loop:true,
+/* spaceBetween:10, // 슬라이드 여백 */
+/* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
 /*     pagination:{
-        el:'.swiper-pagination'
-    }, */
+    el:'.swiper-pagination'
+}, */
+    slidesPerView: 4, //슬라이드 한번에 1개씩 보이기
+    loop:true,
     navigation:{
-        nextEl:'.display_btm_container .swiper-button-next',
-        prevEl:'.display_btm_container .swiper-button-prev',
+        nextEl:'.display_image1 .swiper-button-next',
+        prevEl:'.display_image1 .swiper-button-prev',
     },
 })
 
 /* 전시 서울 */
 const my_swiper_seoul = new Swiper('.display_seoul_container', { 
-    /*     autoplay:{delay:5000}, */
         slidesPerView: 4, //슬라이드 한번에 1개씩 보이기
-        /* spaceBetween:10, // 슬라이드 여백 */
-        /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
-        /* loop:true, */
-    /*     pagination:{
-            el:'.swiper-pagination'
-        }, */
         navigation:{
             nextEl:'.display_seoul_container .swiper-button-next',
             prevEl:'.display_seoul_container .swiper-button-prev',
@@ -166,14 +176,7 @@ const my_swiper_seoul = new Swiper('.display_seoul_container', {
 
 /* 전시 과천 */
 const my_swiper_gwacheon = new Swiper('.display_gwacheon_container', { 
-    /*     autoplay:{delay:5000}, */
-        slidesPerView: 3, //슬라이드 한번에 1개씩 보이기
-        /* spaceBetween:10, // 슬라이드 여백 */
-        /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
-        /* loop:true, */
-    /*     pagination:{
-            el:'.swiper-pagination'
-        }, */
+        slidesPerView: 4, //슬라이드 한번에 1개씩 보이기
         navigation:{
             nextEl:'.display_gwacheon_container .swiper-button-next',
             prevEl:'.display_gwacheon_container .swiper-button-prev',
@@ -182,14 +185,8 @@ const my_swiper_gwacheon = new Swiper('.display_gwacheon_container', {
 
     /* 전시 덕수궁 */
     const my_swiper_deoksugung= new Swiper('.display_deoksugung_container', { 
-        /*     autoplay:{delay:5000}, */
             slidesPerView: 3, //슬라이드 한번에 1개씩 보이기
-            /* spaceBetween:10, // 슬라이드 여백 */
-            /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
             loop:true,
-        /*     pagination:{
-                el:'.swiper-pagination'
-            }, */
             navigation:{
                 nextEl:'.display_deoksugung_container .swiper-button-next',
                 prevEl:'.display_deoksugung_container .swiper-button-prev',
@@ -198,14 +195,8 @@ const my_swiper_gwacheon = new Swiper('.display_gwacheon_container', {
 
     /* 전시 청주 */
     const my_swiper_cheongju= new Swiper('.display_cheongju_container', { 
-        /*     autoplay:{delay:5000}, */
-            slidesPerView: 3, //슬라이드 한번에 1개씩 보이기
-            /* spaceBetween:10, // 슬라이드 여백 */
-            /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
+            slidesPerView: 4, //슬라이드 한번에 1개씩 보이기
             loop:true,
-        /*     pagination:{
-                el:'.swiper-pagination'
-            }, */
             navigation:{
                 nextEl:'.display_cheongju_container .swiper-button-next',
                 prevEl:'.display_cheongju_container .swiper-button-prev',
@@ -214,14 +205,8 @@ const my_swiper_gwacheon = new Swiper('.display_gwacheon_container', {
 
     /* 전시 어린이미술관 */
     const my_swiper_children= new Swiper('.display_children_container', { 
-        /*     autoplay:{delay:5000}, */
             slidesPerView: 1, //슬라이드 한번에 1개씩 보이기
-            /* spaceBetween:10, // 슬라이드 여백 */
-            /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
             loop:true,
-        /*     pagination:{
-                el:'.swiper-pagination'
-            }, */
             navigation:{
                 nextEl:'.display_children_container .swiper-button-next',
                 prevEl:'.display_children_container .swiper-button-prev',
@@ -232,14 +217,8 @@ const my_swiper_gwacheon = new Swiper('.display_gwacheon_container', {
 
 /* 소장품 */
 const my_swiper_collection = new Swiper('.collection_container', { 
-    /*     autoplay:{delay:5000}, */
         slidesPerView: 3, //슬라이드 한번에 1개씩 보이기
-        /* spaceBetween:20,  // 슬라이드 여백
-        /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
         loop:true,
-    /*     pagination:{
-            el:'.swiper-pagination'
-        }, */
         navigation:{
             nextEl:'.collection_container .swiper-button-next',
             prevEl:'.collection_container .swiper-button-prev',
@@ -247,22 +226,12 @@ const my_swiper_collection = new Swiper('.collection_container', {
     })
 
 /* 뉴스레터 */
-/* 소장품 */
-/*  display_btm_container */
-/* 전시 쪽 스와이프 */
 const my_swiper_news = new Swiper('.news_container', { 
-    /*     autoplay:{delay:5000}, */
         slidesPerView: 3, //슬라이드 한번에 1개씩 보이기
-        /* spaceBetween:30, */ // 슬라이드 여백
-        /* centeredSlides:true, */ // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
         loop:true,
-    /*     pagination:{
-            el:'.swiper-pagination'
-        }, */
         navigation:{
-            nextEl:'.new_display_wrap .swiper-button-next',
-            prevEl:'.new_display_wrap .swiper-button-prev',
+            nextEl:'.news_container .swiper-button-next',
+            prevEl:'.news_container .swiper-button-prev',
         }    
     })
-
 
